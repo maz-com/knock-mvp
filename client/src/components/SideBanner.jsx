21c0d24305fce7a0f8fc9eb77cef9de7bcbcc5b2
Mara Caldarini <mara.caldarini@gmail.com>
2024-11-09 00:11:54 +0000

    added styling functionality for cleaning filter button when clicked

--- client/src/components/SideBanner.jsx
+++ client/src/components/SideBanner.jsx
@@ -1,30 +1,66 @@
+import { useState, useEffect } from "react";
 import Badge from "react-bootstrap/Badge";
 import "./SideBanner.css";
 
 const SideBanner = () => {
+  //button status unlcicked
+  //on click of button 1
+  //change button status to clicked
+  //if button status is clicked
+  //fetch all items with category of 1 and store in var result
+  //update setItems to result
+  //if button status is unclicked
+  //fetch all items
+  //update setItems
+
+  const [filteredCleaning, setFilteredCleaning] = useState(false);
+
+  //update state of filteredCleaning to true/false
+  const updateCleaningFilter = () => {
+    /*if (filteredCleaning) {
+        try {
+            //communcate with databasa
+            let response = await axios.get("/api/items/category/:category_id");
+            setItems(response.data);
+          } catch (error) {
+            // handle errors
+            console.error(error);
+          }
+    } */
+
+    //console.log(!filteredCleaning);
+    setFilteredCleaning(!filteredCleaning);
+  };
+
   return (
-    <div class="filter-menu-container">
+    <div className="filter-menu-container">
       <h3 className="filter-title">Filter by category</h3>
 
-      <button class="cleaning-button categories" role="button">
+      <button
+        role="button"
+        onClick={() => updateCleaningFilter()}
+        className={`cleaning-button categories ${
+          filteredCleaning ? "clicked" : null
+        }`}
+      >
         Cleaning
       </button>
-      <button class="DIY-button categories" role="button">
+      <button className="DIY-button categories" role="button">
         DIY
       </button>
-      <button class="gardening-button categories" role="button">
+      <button className="gardening-button categories" role="button">
         Gardening
       </button>
-      <button class="outdoors-button categories" role="button">
+      <button className="outdoors-button categories" role="button">
         Outdoors
       </button>
-      <button class="cooking-button categories" role="button">
+      <button className="cooking-button categories" role="button">
         Cooking
       </button>
-      <button class="audiovisual-button categories" role="button">
+      <button className="audiovisual-button categories" role="button">
         Audiovisual
       </button>
-      <button class="games-hobbies-button categories" role="button">
+      <button className="games-hobbies-button categories" role="button">
         Games & Hobbies
       </button>
     </div>
